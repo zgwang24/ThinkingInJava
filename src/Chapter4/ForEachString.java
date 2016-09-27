418dae90b62a8c4e70236c0a38d0c6d22942a61f
wangzhiguo <zgwang24@163.com>
2016-09-27 11:04:25 +0800

    update project

---
+++ src/Chapter4/ForEachString.java
@@ -0,0 +1,9 @@
+package Chapter4;
+
+public class ForEachString {
+	public static void main(String[] args) {
+		for(char c : "An African Swallow".toCharArray()){
+			System.out.print(c + "");
+		}
+	}
+}
