e8d2b02871131fb360090392505aea470cb6287b
wangzhiguo <zgwang24@163.com>
2016-10-12 09:58:51 +0800

    update project

---
+++ src/Chapter10/Destination.java
@@ -0,0 +1,5 @@
+package Chapter10;
+
+public interface Destination {
+	String readLabel();
+}
---
+++ src/Chapter10/Parcel7b.java
@@ -0,0 +1,18 @@
+package Chapter10;
+
+public class Parcel7b {
+	class MyContents implements Contents{
+		private int i = 11;
+		public int value(){
+			return i;
+		}
+	}
+	public Contents contents(){
+		return new MyContents();
+	}
+	public static void main(String[] args) {
+		Parcel7 p = new Parcel7();
+		Contents c = p.contents();
+		System.out.println(c.value());
+	}
+}
